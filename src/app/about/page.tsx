317425006241a0b9d68872b199c7072ef22f0334
enuesaa <nsrciog@gmail.com>
2022-11-08 23:42:46 +0900

    fix: emotion を使えるようにした

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,17 @@
-// import { css } from '@emotion/react'
+/** @jsxImportSource @emotion/react */
+'use client'
+import { css } from '@emotion/react'
 
 export default function Page() {
-  // const style = {
-  //   h1: css({
-  //     color: '#ff6633',
-  //   })
-  // }
+  const style = {
+    h1: css({
+      color: '#ff6633',
+    })
+  }
 
   return (
     <>
-      <h1>about</h1>
+      <h1 css={style.h1}>about</h1>
     </>
   )
 }
