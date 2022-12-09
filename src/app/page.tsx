f6506614cf5e772fb99938dde121e3075b06251b
enuesaa <nsrciog@gmail.com>
2022-12-10 01:30:17 +0900

    fix: とりあえず動くようにした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
 export default function Page() {
-  return <h1>nextjs13</h1>
+  const style = {
+    h1: css({
+      color: '#ff6633',
+    }),
+  }
+
+  // @ts-ignore
+  return <h1 css={style.h1}>nextjs13</h1>
 }
