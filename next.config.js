a39f5126e71b8861073d0d8c3cdbd6bf4d45fa8f
enuesaa <nsrciog@gmail.com>
2023-10-24 01:25:39 +0900

    fix: nextjs config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
-  experimental: {
-    serverActions: true,
-    typedRoutes: true,
-  },
 }
 
 module.exports = nextConfig
