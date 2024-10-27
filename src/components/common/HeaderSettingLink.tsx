7a1b9da9173716b77e0568fff64c38cac958a48a
nua <enuesaa@gmail.com>
2024-10-27 19:35:52 +0900

    feat(ui): use clsx to write long class names.

--- src/components/common/HeaderSettingLink.tsx
+++ src/components/common/HeaderSettingLink.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
 import { AiTwotoneSetting } from 'react-icons/ai'
+import clsx from 'clsx'
+
+const styles = {
+  link: clsx('inline-block', 'p-3', 'rounded', 'hover:bg-grayer'),
+}
 
 export const HeaderSettingLink = () => {
   return (
-    <Link href='/setting' className='inline-block p-3 rounded hover:bg-grayer'>
+    <Link href='/setting' className={styles.link}>
       <AiTwotoneSetting className='align-middle inline' />
     </Link>
   )
