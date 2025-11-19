47fb93d01cf3b7e46d69963c825c3b535ca10ed0
nua <enuesaa@gmail.com>
2025-11-19 23:25:03 +0900

    feat: migrate tailwind config to globals.css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang='ja'>
       <Head />
-      <body className='text-black bg-gray-200 font-zenkaku'>
+      <body className='text-blac bg-gray-200 font-zenkaku'>
         <Main />
         <NextScript />
       </body>
