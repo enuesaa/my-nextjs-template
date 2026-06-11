01696ed7faf943eae8207bd5a91746bdec7dd266
nua <enuesaa@gmail.com>
2026-06-11 20:03:12 +0900

    chore: remove atmark alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import '@/styles/globals.css'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
