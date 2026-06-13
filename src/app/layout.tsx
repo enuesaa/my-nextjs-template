74e763b5dfa2d3e7b2526e36298dde89f680dcb8
nua <enuesaa@gmail.com>
2026-06-13 15:32:15 +0900

    feat: use app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { ReactNode } from 'react'
+import { Providers } from './providers'
+import '../styles/globals.css'
+
+export const metadata: Metadata = {
+  title: 'my-nextjs-template',
+}
+
+type Props = {
+  children: ReactNode
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang='ja'>
+      <body className='text-blac bg-gray-300 font-zenkaku'>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
